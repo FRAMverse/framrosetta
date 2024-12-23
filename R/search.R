afe9e9ad2ad31b54a21d122e6f26a0ab6203fdc6
Collin Edwards <collin.edwards@dfw.wa.gov>
2024-12-23 14:33:31 -0800

    Added fishery search and stock search functions. Updated DESCRIPTION

---
+++ R/search.R
@@ -0,0 +1,54 @@
+#' Search through fishery and stock lookup tables.
+#'
+#' @param pattern Character string of pattern to search for, case insensitive. If numeric instead, function will return the row with the corresponding stock or fishery ID.
+#' @param species "CHINOOK" or "COHO
+#'
+#' @export
+#'
+#' @examples
+#' fishery_search("kmz", "COHO")
+#' fishery_search(50, "COHO")
+#' stock_search("nooksack", "CHINOOK")
+#' stock_search(21, "CHINOOK")
+
+
+fishery_search = function(pattern, species){
+  species.vec = c("CHINOOK", "COHO")
+  rlang::arg_match(species, species.vec)
+
+  if(species == "CHINOOK"){
+    lut = framrosetta::fishery_chinook_fram
+  }
+  if(species == "COHO"){
+    lut = framrosetta::fishery_coho_fram
+  }
+  if(is.numeric(pattern)){
+    ind = lut$fishery_id == pattern
+  } else {
+    ind = grepl(pattern, lut$fishery_name, ignore.case = T) |
+      grepl(pattern, lut$fishery_title, ignore.case = T)
+  }
+  return(lut[ind,])
+}
+
+
+#' @rdname fishery_search
+#' @export
+stock_search = function(pattern, species){
+  species.vec = c("CHINOOK", "COHO")
+  rlang::arg_match(species, species.vec)
+
+  if(species == "CHINOOK"){
+    lut = framrosetta::stock_chinook_fram
+  }
+  if(species == "COHO"){
+    lut = framrosetta::stock_coho_fram
+  }
+  if(is.numeric(pattern)){
+    ind = lut$stock_id == pattern
+  } else {
+    ind = grepl(pattern, lut$stock_name, ignore.case = T) |
+      grepl(pattern, lut$stock_long_name, ignore.case = T)
+  }
+  return(lut[ind,])
+}
